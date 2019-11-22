6bc59d5e72ecc9eee83e1cb832ed3ca8e1a4919c
Xinyan Wu <wxinyan@wxinyan-a01.vmware.com>
2019-11-21 16:14:52 -0800

    Add crd and auto generated API

---
+++ pkg/apis/veleroplugin/v1/upload.go
@@ -0,0 +1,101 @@
+package v1
+
+import (
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// UploadSpec is the specification for Upload resource
+type UploadSpec struct {
+	// Node is the name of the node that the Pod is running on.
+	Node string `json:"node"`
+
+	// SnapshotID is the identifier for the snapshot of the pod volume.
+	SnapshotID string `json:"snapshotID,omitempty"`
+
+	// Volume is the name of the volume within the Pod to be restored.
+	Volume string `json:"volume"`
+}
+
+// UploadPhase represents the lifecycle phase of a Upload.
+// +kubebuilder:validation:Enum=New;InProgress;Completed;Failed
+type UploadPhase string
+
+const (
+	UploadPhaseNew        UploadPhase = "New"
+	UploadPhaseInProgress UploadPhase = "InProgress"
+	UploadPhaseCompleted  UploadPhase = "Completed"
+	UploadPhaseFailed     UploadPhase = "Failed"
+)
+
+// UploadStatus is the current status of a Upload.
+type UploadStatus struct {
+	// Phase is the current state of the Upload.
+	// +optional
+	Phase UploadPhase `json:"phase,omitempty"`
+
+	// Message is a message about the upload's status.
+	// +optional
+	Message string `json:"message,omitempty"`
+
+	// StartTimestamp records the time a backup was started.
+	// Separate from CreationTimestamp, since that value changes
+	// on restores.
+	// The server's time is used for StartTimestamps
+	// +optional
+	// +nullable
+	StartTimestamp *meta_v1.Time `json:"startTimestamp,omitempty"`
+
+	// CompletionTimestamp records the time a backup was completed.
+	// Completion time is recorded even on failed backups.
+	// Completion time is recorded before uploading the backup object.
+	// The server's time is used for CompletionTimestamps
+	// +optional
+	// +nullable
+	CompletionTimestamp *meta_v1.Time `json:"completionTimestamp,omitempty"`
+
+	// Progress holds the total number of bytes of the volume and the current
+	// number of backed up bytes. This can be used to display progress information
+	// about the backup operation.
+	// +optional
+	Progress UploadOperationProgress `json:"progress,omitempty"`
+}
+
+// UploadOperationProgress represents the progress of a
+// Upload operation
+type UploadOperationProgress struct {
+	// +optional
+	TotalBytes int64 `json:"totalBytes,omitempty"`
+
+	// +optional
+	BytesDone int64 `json:"bytesDone,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Upload describe a velero-plugin backup
+type Upload struct {
+	// TypeMeta is the metadata for the resource, like kind and apiversion
+	meta_v1.TypeMeta `json:",inline"`
+
+	// +optional
+	meta_v1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Spec is the custom resource spec
+	Spec UploadSpec `json:"spec"`
+
+	// +optional
+	Status UploadStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// UploadList is a list of Upload resources
+type UploadList struct {
+	meta_v1.TypeMeta `json:",inline"`
+
+	// +optional
+	meta_v1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Upload `json:"items"`
+}
