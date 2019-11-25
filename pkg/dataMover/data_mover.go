f30ca627c39cba365512796fcf600bc88e992412
Lintong Jiang <lintongj@vmware.com>
2019-11-25 14:25:41 -0800

    Added the impl of upload controller

---
+++ pkg/dataMover/data_mover.go
@@ -0,0 +1,153 @@
+package dataMover
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/sirupsen/logrus"
+	"github.com/vmware-tanzu/astrolabe/pkg/astrolabe"
+	"github.com/vmware-tanzu/astrolabe/pkg/ivd"
+	"github.com/vmware-tanzu/astrolabe/pkg/s3repository"
+	"github.com/vmware-tanzu/velero-plugin-for-vsphere/pkg/utils"
+	"net/url"
+)
+
+type DataMover struct {
+	logrus.FieldLogger
+	ivdPETM *ivd.IVDProtectedEntityTypeManager
+	s3PETM  *s3repository.ProtectedEntityTypeManager
+}
+
+func NewDataMoverFromCluster(logger logrus.FieldLogger) (*DataMover, error) {
+	params := make(map[string]interface{})
+	var err error
+	// hardcode VC credential for debug purpose
+	err = utils.RetrieveVcConfigSecretByHardCoding(params, logger)
+	//err = utils.RetrieveVcConfigSecret(params, logger)
+	if err != nil {
+		logger.Errorf("Could not retrieve vsphere credential from k8s secret with error message: %v", err)
+		return nil, err
+	}
+
+	err = utils.RetrieveBackupStorageLocation(params, logger)
+	if err != nil {
+		logger.Errorf("Could not retrieve velero default backup location with error message: %v", err)
+		return nil, err
+	}
+
+	dataMover, err := newDataMoverFromParamsMap(params, logger)
+	if err != nil {
+		logger.Errorf("Failed to new a snapshot manager from params map")
+		return nil, err
+	}
+	return dataMover, nil
+}
+
+func newDataMoverFromParamsMap(params map[string]interface{}, logger logrus.FieldLogger) (*DataMover, error) {
+	var vcUrl url.URL
+	vcUrl.Scheme = "https"
+	vcHostStr, ok := params["VirtualCenter"].(string)
+	if !ok {
+		return nil, errors.New("Missing vcHost param, cannot initialize SnapshotManager")
+	}
+	vcHostPortStr, ok := params["port"].(string)
+	if !ok {
+		return nil, errors.New("Missing port param, cannot initialize SnapshotManager")
+	}
+
+	vcUrl.Host = fmt.Sprintf("%s:%s", vcHostStr, vcHostPortStr)
+
+	vcUser, ok := params["user"].(string)
+	if !ok {
+		return nil, errors.New("Missing vcUser param, cannot initialize SnapshotManager")
+	}
+	vcPassword, ok := params["password"].(string)
+	if !ok {
+		return nil, errors.New("Missing vcPassword param, cannot initialize SnapshotManager")
+	}
+	vcUrl.User = url.UserPassword(vcUser, vcPassword)
+	vcUrl.Path = "/sdk"
+
+	insecure := false
+	insecureStr, ok := params["insecure-flag"].(string)
+	if ok && (insecureStr == "TRUE" || insecureStr == "true") {
+		insecure = true
+	}
+
+	region := params["region"].(string)
+	bucket := params["bucket"].(string)
+
+	s3URLBase := fmt.Sprintf("https://s3-%s.amazonaws.com/%s/", region, bucket)
+	serviceType := "ivd"
+
+	// init ivd PETM
+	ivdPETM, err := ivd.NewIVDProtectedEntityTypeManagerFromURL(&vcUrl, s3URLBase, insecure, logger)
+	if err != nil {
+		logger.Errorf("Error at calling ivd API, NewIVDProtectedEntityTypeManagerFromURL")
+		return nil, err
+	}
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	}))
+
+	s3PETM, err := s3repository.NewS3RepositoryProtectedEntityTypeManager(serviceType, *sess, bucket, logger)
+	if err != nil {
+		logger.Errorf("Error at creating new ProtectedEntityTypeManager for s3 repository")
+		return nil, err
+	}
+
+	dataMover := DataMover{
+		FieldLogger: logger,
+		ivdPETM: ivdPETM,
+		s3PETM:  s3PETM,
+	}
+
+	logger.Infof("Data Moving Manager is initialized")
+	return &dataMover, nil
+}
+
+func (this *DataMover) CopyToRepo(peID astrolabe.ProtectedEntityID) (astrolabe.ProtectedEntityID, error) {
+	this.Infof("Copying the snapshot, %s, from local to remote repository", peID.String())
+	ctx := context.Background()
+	updatedPE, err := this.ivdPETM.GetProtectedEntity(ctx, peID)
+	if err != nil {
+		this.Errorf("Failed to GetProtectedEntity for, %s, with error message, %v", peID.String(), err)
+		return astrolabe.ProtectedEntityID{}, err
+	}
+
+	this.Infof("Ready to call s3 PETM copy API")
+	s3PE, err := this.s3PETM.Copy(ctx, updatedPE, astrolabe.AllocateNewObject)
+	this.Infof("Return from the call of s3 PETM copy API")
+	if err != nil {
+		this.Errorf("Failed at copying to remote repository for, %s, with error message, %v",
+			peID.String(), err)
+		return astrolabe.ProtectedEntityID{}, err
+	}
+
+	this.Infof("Protected Entity, %s, was just copied from local to remote repository as, %s", peID.String(), s3PE.GetID().String())
+	return s3PE.GetID(), nil
+}
+
+func (this *DataMover) CopyFromRepo(peID astrolabe.ProtectedEntityID) (astrolabe.ProtectedEntityID, error) {
+	this.Infof("Copying the snapshot, %s, from remote repository to local", peID.String())
+	ctx := context.Background()
+	pe, err := this.s3PETM.GetProtectedEntity(ctx, peID)
+	if err != nil {
+		this.Errorf("Failed to GetProtectedEntity for, %s, with error message, %v", peID.String(), err)
+		return astrolabe.ProtectedEntityID{}, err
+	}
+
+	this.Infof("Ready to call ivd PETM copy API")
+	ivdPE, err := this.ivdPETM.Copy(ctx, pe, astrolabe.AllocateNewObject)
+	this.Infof("Return from the call of ivd PETM copy API")
+	if err != nil {
+		this.Errorf("Failed to copy from remote repository for, %s, with error message, %v", peID.String(), err)
+		return astrolabe.ProtectedEntityID{}, err
+	}
+
+	this.Infof("Protected Entity, %s, was just copied from remote repository to local as, %s", peID.String(), ivdPE.GetID().String())
+	return ivdPE.GetID(), nil
+}
---
+++ pkg/utils/utils.go
@@ -0,0 +1,79 @@
+package utils
+
+import (
+	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"strings"
+	"github.com/vmware-tanzu/velero/pkg/generated/clientset/versioned"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func RetrieveBackupStorageLocation(params map[string]interface{}, logger logrus.FieldLogger) error {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return err
+	}
+
+	veleroClient, err := versioned.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	defaultBackupLocation := "default"
+	backupStorageLocation, err := veleroClient.VeleroV1().BackupStorageLocations("velero").
+		Get(defaultBackupLocation, metav1.GetOptions{})
+	params["region"] = backupStorageLocation.Spec.Config["region"]
+	params["bucket"] = backupStorageLocation.Spec.ObjectStorage.Bucket
+
+	return nil
+}
+
+/*
+ * In the CSI setup, VC credential is stored as a secret
+ * under the kube-system namespace.
+ */
+func RetrieveVcConfigSecret(params map[string]interface{}, logger logrus.FieldLogger) error {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		logger.Errorf("Failed to get k8s inClusterConfig")
+		return err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		logger.Errorf("Failed to get k8s clientset with the given config")
+		return err
+	}
+
+	ns := "kube-system"
+	secretApis := clientset.CoreV1().Secrets(ns)
+	vsphere_secret := "vsphere-config-secret"
+	secret, err := secretApis.Get(vsphere_secret, metav1.GetOptions{})
+	if err != nil {
+		logger.Errorf("Failed to get k8s secret, %s", vsphere_secret)
+		return err
+	}
+	sEnc := string(secret.Data["csi-vsphere.conf"])
+	lines := strings.Split(sEnc, "\n")
+
+	for _, line := range lines {
+		if strings.Contains(line, "VirtualCenter") {
+			parts := strings.Split(line, "\"")
+			params["VirtualCenter"] = parts[1]
+		} else if strings.Contains(line, "=") {
+			parts := strings.Split(line, "=")
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			params[key] = value[1 : len(value)-1]
+		}
+	}
+	return nil
+}
+
+
+func RetrieveVcConfigSecretByHardCoding(params map[string]interface{}, logger logrus.FieldLogger) error {
+	params["VirtualCenter"] = "10.193.45.3"
+	params["port"] = "443"
+	params["user"] = "administrator@vsphere.local"
+	params["password"] = "Admin!23"
+	return nil
+}
