8bd2d50dc628cdbebbec876337d4a356d1ca1bd5
Lintong Jiang <lintongj@vmware.com>
2020-03-04 11:35:21 -0800

    Remove the version setting from the script to push image based on the recent change in Makefile
    Signed-off-by: Lintong Jiang <lintongj@vmware.com>

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -6,13 +6,9 @@ then
   exit 1
 fi
 
-BRANCH=$1
-COMMIT=$(git log --oneline | head -1 | awk '{print $1}')
-TIMESTAMP=$(date +%s)
-
-if [ $BRANCH = "v0.9.0"  ]
+if [ $BRANCH = "0.9.0"  ]
 then
-  make push LOCALMODE=true VERSION=$BRANCH-$COMMIT-$TIMESTAMP
+  make push LOCALMODE=true
 else
-  make push VERSION=$BRANCH-$COMMIT-$TIMESTAMP
+  make push
 fi
